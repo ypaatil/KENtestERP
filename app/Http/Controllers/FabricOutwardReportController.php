12384b6c2fa8fe747b86a8d908908bb34b57f5c1
Nutan Rajendra Sawant <nutansawant28042001@gmail.com>
2025-11-08 15:59:08 +0530

    Fabric Outward Rolls Print

--- app/Http/Controllers/FabricOutwardReportController.php
+++ app/Http/Controllers/FabricOutwardReportController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\DB;
 use DateTime;
 use DatePeriod;
 use DateInterval;
+
 class FabricOutwardReportController extends Controller
 {
     /**
@@ -18,8 +19,8 @@ class FabricOutwardReportController extends Controller
      */
     public function index()
     {
-      
-      return view('FabricOutwardReport'); 
+
+        return view('FabricOutwardReport');
     }
 
     /**
@@ -40,34 +41,32 @@ class FabricOutwardReportController extends Controller
      */
     public function store(Request $request)
     {
-        
-           $fdate=$request->fdate;
-           $tdate=$request->tdate;
-      
-         $FabricOutwardMaster = FabricOutwardModel::join('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
-         ->join('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
-          
-        ->whereBetween('fabric_outward_master.fout_date', [$request->fdate, $request->tdate])
-         ->get(['fabric_outward_master.*','usermaster.username','ledger_master.Ac_name' ]);
-     // $query = DB::getQueryLog();
-     //     $query = end($query);
-     //     dd($query);
-     
-         $fout_code=0;
-         foreach ($FabricOutwardMaster as $rowfetch){ 
-             
-                      $fout_code=$rowfetch->fout_code;
 
-                 }
-     
-     
-           $FabricOutwardDetailstable = FabricOutwardDetailModel:: join('item_master','item_master.item_code', '=', 'fabric_outward_details.item_code')
-             
-            ->join('part_master','part_master.part_id', '=', 'fabric_outward_details.part_id')
-            ->where('fabric_outward_details.fout_code','=', $fout_code)->get(['fabric_outward_details.*', 'item_master.color_name','item_master.item_description','item_master.dimension','part_master.part_name']);
-      
-         return view('rptFabricOutwardReport', compact('FabricOutwardMaster','FabricOutwardDetailstable'));
-    
+        $fdate = $request->fdate;
+        $tdate = $request->tdate;
+
+        $FabricOutwardMaster = FabricOutwardModel::join('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
+            ->join('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
+
+            ->whereBetween('fabric_outward_master.fout_date', [$request->fdate, $request->tdate])
+            ->get(['fabric_outward_master.*', 'usermaster.username', 'ledger_master.Ac_name']);
+        // $query = DB::getQueryLog();
+        //     $query = end($query);
+        //     dd($query);
+
+        $fout_code = 0;
+        foreach ($FabricOutwardMaster as $rowfetch) {
+
+            $fout_code = $rowfetch->fout_code;
+        }
+
+
+        $FabricOutwardDetailstable = FabricOutwardDetailModel::join('item_master', 'item_master.item_code', '=', 'fabric_outward_details.item_code')
+
+            ->join('part_master', 'part_master.part_id', '=', 'fabric_outward_details.part_id')
+            ->where('fabric_outward_details.fout_code', '=', $fout_code)->get(['fabric_outward_details.*', 'item_master.color_name', 'item_master.item_description', 'item_master.dimension', 'part_master.part_name']);
+
+        return view('rptFabricOutwardReport', compact('FabricOutwardMaster', 'FabricOutwardDetailstable'));
     }
 
     /**
@@ -114,122 +113,139 @@ class FabricOutwardReportController extends Controller
     {
         //
     }
-    
+
     public function GetFabricInOutStockReportForm()
     {
-       return view('GetFabricInOutStockReport'); 
+        return view('GetFabricInOutStockReport');
     }
-    
+
     public function GetFabricInOutStockSummaryReport()
     {
-       return view('GetFabricInOutStockSummaryReport'); 
+        return view('GetFabricInOutStockSummaryReport');
     }
-    
-    
-    public function FabricInOutStockSummaryReport (Request $request)
+
+
+    public function FabricInOutStockSummaryReport(Request $request)
     {
-        
-        $fdate= $request->fdate;
-        $tdate= $request->tdate;
-         
-        if($tdate>date('Y-m-d')){$tdate=date('Y-m-d');}
-         
-         
+
+        $fdate = $request->fdate;
+        $tdate = $request->tdate;
+
+        if ($tdate > date('Y-m-d')) {
+            $tdate = date('Y-m-d');
+        }
+
+
         $period = $this->getBetweenDates($fdate, $tdate);
-           
-        
+
+
         $FirmDetail =  DB::table('firm_master')->first();
-  
-        return view('FabricInOutStockSummaryReport', compact('period','fdate', 'tdate','FirmDetail'));
-      
+
+        return view('FabricInOutStockSummaryReport', compact('period', 'fdate', 'tdate', 'FirmDetail'));
     }
-    
-    
+
+
     public  function getBetweenDates($startDate, $endDate)
     {
         $rangArray = [];
-    
+
         $startDate = strtotime($startDate);
         $endDate = strtotime($endDate);
-    
+
         for (
             $currentDate = $startDate;
             $currentDate <= $endDate;
             $currentDate += (86400)
         ) {
-    
+
             $date = date('Y-m-d', $currentDate);
             $rangArray[] = $date;
         }
-    
+
         return $rangArray;
     }
-        
-    public function getFabricInOutStockReport (Request $request)
+
+    public function getFabricInOutStockReport(Request $request)
     {
-        
-         $fdate= $request->fdate;
-         $tdate= $request->tdate;
-         
-         if($tdate>date('Y-m-d')){$tdate=date('Y-m-d');}
-         
-         
-         $period = $this->getBetweenDates($fdate, $tdate);
-           
+
+        $fdate = $request->fdate;
+        $tdate = $request->tdate;
+
+        if ($tdate > date('Y-m-d')) {
+            $tdate = date('Y-m-d');
+        }
+
+
+        $period = $this->getBetweenDates($fdate, $tdate);
+
         $FirmDetail =  DB::table('firm_master')->first();
-          
-    //   DB::enableQueryLog();
-          
+
+        //   DB::enableQueryLog();
+
         //     $query = DB::getQueryLog();
         //   $query = end($query);
         //   dd($query);
-             return view('FabricInOutStockPrint', compact('period','fdate', 'tdate','FirmDetail'));
-      
+        return view('FabricInOutStockPrint', compact('period', 'fdate', 'tdate', 'FirmDetail'));
     }
-    
-     public function FabricOutwardPrint ($fout_code)
+
+    public function FabricOutwardPrint($fout_code)
     {
-         $FirmDetail =  DB::table('firm_master')->first();
-         $fout_code=base64_decode($fout_code);
-    //   DB::enableQueryLog();
-       
+        $FirmDetail =  DB::table('firm_master')->first();
+        $fout_code = base64_decode($fout_code);
+        //   DB::enableQueryLog();
+
         $FabricOutwardMaster = FabricOutwardModel::leftJoin('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
-        ->leftJoin('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
-        ->leftJoin('vendor_purchase_order_master', 'vendor_purchase_order_master.vpo_code', '=','fabric_outward_master.vpo_code')
-        ->where('fabric_outward_master.fout_code', $fout_code)
-        ->get(['fabric_outward_master.*','usermaster.username','ledger_master.Ac_name','vendor_purchase_order_master.sales_order_no','ledger_master.gst_no','ledger_master.pan_no','ledger_master.state_id','ledger_master.address' ]);
-       
+            ->leftJoin('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
+            ->leftJoin('vendor_purchase_order_master', 'vendor_purchase_order_master.vpo_code', '=', 'fabric_outward_master.vpo_code')
+            ->where('fabric_outward_master.fout_code', $fout_code)
+            ->get(['fabric_outward_master.*', 'usermaster.username', 'ledger_master.Ac_name', 'vendor_purchase_order_master.sales_order_no', 'ledger_master.gst_no', 'ledger_master.pan_no', 'ledger_master.state_id', 'ledger_master.address']);
+
         //     $query = DB::getQueryLog();
         //   $query = end($query);
         //   dd($query);
-        
-             return view('FabricOutwardPrint', compact('FabricOutwardMaster','FirmDetail'));
-      
+
+        return view('FabricOutwardPrint', compact('FabricOutwardMaster', 'FirmDetail'));
     }
-    
-     public function FabricOutwardRollsPrint ($fout_code)
+
+
+
+    public function FabricOutwardRollsPrint($fout_code)
     {
-        
-         $fout_code=base64_decode($fout_code);
-    //   DB::enableQueryLog();
+
+        $fout_code = base64_decode($fout_code);
+        //   DB::enableQueryLog();
         $FirmDetail =  DB::table('firm_master')->first();
-         $FabricOutwardMaster = FabricOutwardModel::join('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
-         ->leftJoin('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
-         ->leftJoin('vendor_purchase_order_master', 'vendor_purchase_order_master.vpo_code', '=','fabric_outward_master.vpo_code')
-           ->join('main_style_master', 'main_style_master.mainstyle_id', '=', 'fabric_outward_master.mainstyle_id', 'left outer')
-           ->join('sub_style_master', 'sub_style_master.substyle_id', '=', 'fabric_outward_master.substyle_id', 'left outer')
+        $FabricOutwardMaster = FabricOutwardModel::join('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
+            ->leftJoin('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
+            ->leftJoin('vendor_purchase_order_master', 'vendor_purchase_order_master.vpo_code', '=', 'fabric_outward_master.vpo_code')
+            ->join('main_style_master', 'main_style_master.mainstyle_id', '=', 'fabric_outward_master.mainstyle_id', 'left outer')
+            ->join('sub_style_master', 'sub_style_master.substyle_id', '=', 'fabric_outward_master.substyle_id', 'left outer')
             ->join('fg_master', 'fg_master.fg_id', '=', 'fabric_outward_master.fg_id', 'left outer')
-        ->where('fabric_outward_master.fout_code', $fout_code)
-         ->get(['fabric_outward_master.*','usermaster.username','ledger_master.Ac_name','vendor_purchase_order_master.sales_order_no','ledger_master.gst_no','ledger_master.pan_no','ledger_master.state_id','ledger_master.address','fg_master.fg_name','main_style_master.mainstyle_name' ,'sub_style_master.substyle_name']);
-       
+            ->where('fabric_outward_master.fout_code', $fout_code)
+            ->get(['fabric_outward_master.*', 'usermaster.username', 'ledger_master.Ac_name', 'vendor_purchase_order_master.sales_order_no', 'ledger_master.gst_no', 'ledger_master.pan_no', 'ledger_master.state_id', 'ledger_master.address', 'fg_master.fg_name', 'main_style_master.mainstyle_name', 'sub_style_master.substyle_name']);
+
         //     $query = DB::getQueryLog();
         //   $query = end($query);
         //   dd($query);
-             return view('FabricOutwardRollsPrint', compact('FabricOutwardMaster','FirmDetail'));
-      
+        return view('FabricOutwardRollsPrint', compact('FabricOutwardMaster', 'FirmDetail'));
     }
-    
-    
-    
-    
+
+    public function FabricOutwardRollsPrintView($fout_code)
+    {
+
+        $fout_code = base64_decode($fout_code);
+        //   DB::enableQueryLog();
+        $FirmDetail =  DB::table('firm_master')->first();
+        $FabricOutwardMaster = FabricOutwardModel::join('usermaster', 'usermaster.userId', '=', 'fabric_outward_master.userId')
+            ->leftJoin('ledger_master', 'ledger_master.Ac_code', '=', 'fabric_outward_master.vendorId')
+            ->leftJoin('vendor_purchase_order_master', 'vendor_purchase_order_master.vpo_code', '=', 'fabric_outward_master.vpo_code')
+            ->join('main_style_master', 'main_style_master.mainstyle_id', '=', 'fabric_outward_master.mainstyle_id', 'left outer')
+            ->join('sub_style_master', 'sub_style_master.substyle_id', '=', 'fabric_outward_master.substyle_id', 'left outer')
+            ->join('fg_master', 'fg_master.fg_id', '=', 'fabric_outward_master.fg_id', 'left outer')
+            ->where('fabric_outward_master.fout_code', $fout_code)
+            ->get(['fabric_outward_master.*', 'usermaster.username', 'ledger_master.Ac_name', 'vendor_purchase_order_master.sales_order_no', 'ledger_master.gst_no', 'ledger_master.pan_no', 'ledger_master.state_id', 'ledger_master.address', 'fg_master.fg_name', 'main_style_master.mainstyle_name', 'sub_style_master.substyle_name']);
+
+
+        return view('FabricOutwardRollsView', compact('FabricOutwardMaster', 'FirmDetail'));
+    }
 }
