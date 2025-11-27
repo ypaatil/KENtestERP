0ffd518347dfdf7de37380b2349117cda16b5bd4
ypaatil <ypaatil@gmail.com>
2025-11-27 17:48:55 +0530

    validation date error added for fabric stock report
    validation date error added for fabric stock report

--- resources/views/GetFabricInOutStockReport.blade.php
+++ resources/views/GetFabricInOutStockReport.blade.php
@@ -18,10 +18,19 @@
     </div>
     </div>
 @endif
- 
+
+<div class="row">
+    <div class="col-12 col-md-6">
+       <div id="dateError"></div>
+    </div>
+    <div class="col-12 col-md-6">       
+    </div>
+</div>
+
 <form action="{{route('FabricInOutStockReport')}}" method="GET" enctype="multipart/form-data">
 @csrf 
 <div class="row">
+
 <div class="col-md-2">
 <div class="mb-3">
 <label for="formrow-email-input" class="form-label">From date</label>
@@ -54,14 +63,26 @@
  
 <script>
 document.querySelector("form").addEventListener("submit", function(e) {
-
     let fromDate = document.getElementById("fdate").value;
     let toDate   = document.getElementById("tdate").value;
 
+    let errorBox = document.getElementById("dateError");
+
+    // Clear previous message
+    errorBox.innerHTML = "";
+
     if (toDate < fromDate) {
-        e.preventDefault(); // stop form submit
-        alert("To Date cannot be less than From Date.");
-    }   
+        e.preventDefault();
+        errorBox.innerHTML = `
+            <div class="alert alert-danger alert-dismissible fade show mt-2" role="alert">
+                <strong>Error:</strong> To Date cannot be less than From Date.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+            </div>
+        `;
+    }
+    else{
+        errorBox.innerHTML = "";
+    }
 });
 </script>
 
